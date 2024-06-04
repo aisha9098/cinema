da89c97dfefe7d329820824868c7ad19754f6a6b
Aisha <shaik.aisha9955@gmail.com>
2024-06-05 01:35:18 +0530

    new data

--- variables.sh
+++ variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "ramesh :Hi Sujan Good morning"
-echo "sujan: Good Morning ramesh"
+PERSON1=ramesh
+PERSON2=sujan
+
+echo "$PERSON1:Hi $PERSON2 Good morning"
+echo "$PERSON1: Good Morning $PERSON2"
 echo "ramesh how are you sujan"
 echo "sujan : iam good how are you"
